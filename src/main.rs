9a8602aeaaf76fd25bc468ee58058a0add3468d1
Olof Nord <olof.nord@tutanota.com>
2019-12-01 21:22:14 +0100

    Adding /customers/customerId endpoint, switching to String return type

--- src/main.rs
+++ src/main.rs
@@ -3,16 +3,21 @@
 #[macro_use] extern crate rocket;
 
 #[get("/customers")]
-fn get_customers() -> &'static str {
-    "List all customers"
+fn get_customers() -> String {
+    format!("List all customers")
 }
 
 #[post("/customers")]
-fn post_customers() -> &'static str {
-    "Create a new customer"
+fn post_customers() -> String {
+    format!("Create a new customer")
+}
+
+#[get("/customers/<customer_id>")]
+fn get_customer(customer_id: String) -> String {
+    format!("Get customer with id {}", customer_id)
 }
 
 fn main() {
     rocket::ignite().mount("/", routes![
-    get_customers, post_customers]).launch();
+    get_customers, post_customers, get_customer]).launch();
 }
